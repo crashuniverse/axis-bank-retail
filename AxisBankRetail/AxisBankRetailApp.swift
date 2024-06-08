486a75bb76e1a86f45bfdc389d174489bdc39c03
Priya Ranjan Singh <priya-ranjan.singh@hpe.com>
2024-06-08 11:54:43 +0530

    Initial Commit

---
+++ AxisBankRetail/AxisBankRetailApp.swift
@@ -0,0 +1,17 @@
+//
+//  AxisBankRetailApp.swift
+//  AxisBankRetail
+//
+//  Created by Priya Ranjan Singh on 08/06/24.
+//
+
+import SwiftUI
+
+@main
+struct AxisBankRetailApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

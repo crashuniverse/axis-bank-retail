c45e75a90774b33f93ab3cc4fe79bf979e9af540
Priya Ranjan Singh <mail@priyaranjan.net>
2024-06-08 13:00:50 +0530

    Add dashboard view

--- AxisBankRetail/ContentView.swift
+++ AxisBankRetail/ContentView.swift
@@ -9,12 +9,38 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(alignment: .leading, spacing: 32) {
+            Text("Dashboard")
+                .font(.system(size: 24))
+                .bold()
+
+            VStack(alignment: .leading, spacing: 8) {
+                VStack(alignment: .leading) {
+                    Text("Welcome John")
+                        .fontWeight(.thin)
+                        .font(.system(size: 16))
+                }
+                
+                VStack(alignment: .leading, spacing: 8) {
+                    Text("Accounts")
+                        .fontWeight(.bold)
+                    
+                    VStack(alignment: .leading) {
+                        Text("7612 1324 2132 768")
+                        Text("Savings bank account")
+                        Text("Balance: Rs. 1,32,000")
+                        Link(destination: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=URL@*/URL(string: "https://www.apple.com")!/*@END_MENU_TOKEN@*/) {
+                            Text("View transactions")
+                        }
+                    }
+                }
+            }
         }
+        .frame(
+            maxWidth: .infinity,
+            maxHeight: .infinity,
+            alignment: .topLeading
+        )
         .padding()
     }
 }
